f69ec96e110c729397751dbebb55d63bb8be8579
Onelore <wen1995cc@outlook.com>
2020-03-18 10:05:39 +0800

    LeetCode练习

--- src/array/Problem1160.java
+++ src/array/Problem1160.java
@@ -1,6 +1,8 @@
 package array;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  * 给你一份『词汇表』（字符串数组） words 和一张『字母表』（字符串） chars。
@@ -26,28 +28,58 @@ import java.util.HashSet;
 public class Problem1160 {
     public static int countCharacters(String[] words, String chars) {
 //        chars = chars.trim();
-        HashSet<Character> set = new HashSet<>();
-        for (int i = 0; i < chars.length(); i++) {
-            set.add(chars.charAt(i));
+        /**1、不符合题意://注意：每次拼写时，chars 中的每个字母都只能用一次。
+         * HashSet<Character> set = new HashSet<>();
+         *         for (int i = 0; i < chars.length(); i++) {
+         *             set.add(chars.charAt(i));
+         *         }
+         *         int wordAns = 0;
+         *         for (int i = 0; i < words.length; i++) {
+         *             int count = 0;
+         *             for (int j = 0; j < words[i].length(); j++) {
+         *                 if (set.contains(words[i].charAt(j))) {
+         *                     count++;
+         *                 }
+         *             }
+         *             if (count == words[i].length()) {
+         *                 wordAns += count;
+         *             }
+         *         }
+         *         return wordAns;
+         */
+
+        /**
+         * 2、
+         * 执行用时 :7 ms, 在所有 Java 提交中击败了83.84%的用户
+         * 内存消耗 :41.4 MB, 在所有 Java 提交中击败了5.08%的用户
+         */
+        int []vab=new int[26];
+        for (char c:chars.toCharArray()){
+            vab[(int)(c-'a')]++;
         }
-        int wordAns = 0;
-        for (int i = 0; i < words.length; i++) {
-            int count = 0;
-            for (int j = 0; j < words[i].length(); j++) {
-                if (set.contains(words[i].charAt(j))) {
-                    count++;
+        int res=0;
+        a:for (String word:words){
+            int []wordVab=new int[26];
+            for (char w:word.toCharArray()){
+                wordVab[(int)(w-'a')]++;
+            }
+            for (int i=0;i<26;i++){
+                if (wordVab[i]>vab[i]){
+                    continue a;
                 }
             }
-            if (count == words[i].length()) {
-                wordAns += count;
-            }
+            res+=word.length();
         }
-        return wordAns;
+        return res;
     }
+
 
     public static void main(String[] args) {
         int a=  countCharacters(new String[]{"dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin","ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb","ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl","boygirdlggnh","xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx","nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop","hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx","juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr","lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo","oxgaskztzroxuntiwlfyufddl","tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp","qnagrpfzlyrouolqquytwnwnsqnmuzphne","eeilfdaookieawrrbvtnqfzcricvhpiv","sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz","yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue","hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv","cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo","teyygdmmyadppuopvqdodaczob","qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqxwekbpfzs","qqxpxpmemkldghbmbyxpkwgkaykaerhmwwjonrhcsubchs"},
         "usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp");
+//        int a=countCharacters(new String[]{"cat","bt","hat","tree"},  "atach");
         System.out.println(a);
+
+
     }
 }
